4de9cd35303845f44dbac7bb69b059b032465e33
technebulaa <technebulaa@nebulaa.in>
2025-05-10 14:06:08 +0530

    chnaged port to default 5000 for running backend

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 
 from routes.chat_api import query_bp
 from routes.update_api import update_bp
 
-from services.scheduler_service import schedule_daily_updates
+# from services.scheduler_service import schedule_daily_updates
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access environment variables
+PORT = int(os.getenv("PORT", 5000))
 
 app = Flask(__name__)
 CORS(app)
@@ -17,4 +25,4 @@ app.register_blueprint(update_bp)
 # schedule_daily_updates()
 
 if __name__ == "__main__":
-    app.run(port=5555, debug=True)
+    app.run(host="0.0.0.0", port=PORT)
